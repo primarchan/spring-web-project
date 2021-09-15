876402300788de125b632f67de86190b50d51320
Taegyun An <boltcarrier@naver.com>
2021-09-15 22:35:20 +0900

    ex02 1st commit

---
+++ ex02/src/test/java/org/taesan/persistence/JDBCTests.java
@@ -0,0 +1,27 @@
+package org.taesan.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTests {
+	
+	@Test
+	public void testConnection() throws Exception {
+		
+		Class clz = Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+			log.info(clz);
+			
+			Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "book_ex", "1111");
+			
+			log.info(conn);
+			
+	}
+	
+
+}
